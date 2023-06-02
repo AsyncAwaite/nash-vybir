12bc87ae714af74cb8a00e7673d7739ff7b3fe45
Async <sunsei288@gmail.com>
2023-06-02 11:23:46 +0300

    updates

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,11 +1,20 @@
 import {header} from "./elements.js";
+
 const burger = () => {
     if (header) {
         const headerNav = header.querySelector(".header");
         const burger = header.querySelector(".burger");
-        if (screen.availWidth <= 991){
-            header.classList.add('--hide')
-        }
+        const close = header.querySelector(".close");
+        // if (screen.availWidth <= 991){
+        //     header.classList.add('--hide')
+        // }
+        close.addEventListener("click", () => {
+            burger.classList.toggle("active");
+            header.classList.toggle("active");
+            // header.classList.toggle('--show');
+            // headerNav.classList.toggle("active");
+            // document.body.classList.toggle("active");
+        });
         burger.addEventListener("click", () => {
             burger.classList.toggle("active");
             header.classList.toggle("active");

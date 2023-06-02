8333171650bdbc34f96ac819ba5ad8f062e33319
Async <sunsei288@gmail.com>
2023-06-02 12:04:19 +0300

    delete scroll

--- src/js/app.js
+++ src/js/app.js
@@ -24,7 +24,6 @@ window.addEventListener('scroll', function () {
 window.addEventListener("DOMContentLoaded", () => {
     try {
         scrollToAnchor();
-
         burger();
         // headerFixed();
 
